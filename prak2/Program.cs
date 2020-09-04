2d6bda78e3ef86add4a6b46c499d64039aadc4d0
Aniska31 <Avstralia08@mail.ru>
2020-09-04 11:21:40 +0300

    finish1

--- prak2/Program.cs
+++ prak2/Program.cs
@@ -6,7 +6,18 @@ namespace prak2
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      SByte a = 0; Byte b = 0; Int16 с = 0; Int32 d = 0; Int64 e = 0; string s = "";
+      Exception ex = new Exception();
+      object[] types = { a, b, с, d, e, s, ex };
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+
+      }
     }
   }
 }
