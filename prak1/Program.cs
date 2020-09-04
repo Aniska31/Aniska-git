d6e5ad2e854bae230f7a9cdfb10c0688ce7abaa9
Aniska31 <Avstralia08@mail.ru>
2020-09-04 10:32:14 +0300

    conti

--- prak1/Program.cs
+++ prak1/Program.cs
@@ -9,19 +9,22 @@ namespace prak1
       public string firstName;
       public string lastName;
       public int age;
-      public Person(string _firstName, string _lastName, int _age)
+      public Genders gender;
+      public Person(string _firstName, string _lastName, int _age, Genders _gender)
       {
-        firstName = _firstName;
-        lastName = _lastName; age = _age;
+        firstName = _firstName; lastName =
+        _lastName; age = _age; gender = _gender;
       }
       public override string ToString()
       {
-        return firstName + " " + lastName + ", Годиков " + age;
+        return firstName + " " + lastName + " (" + gender + "), age " + age;
       }
+      public enum Genders : int { Male, Female };
     }
-    static void Main(string[] args)
-    {
-        Person p = new Person("Анисимов", "Егор", 19); Console.WriteLine(p);
+      static void Main(string[] args)
+      {
+        Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+        Console.WriteLine(p.ToString());
       }
   }
 }
