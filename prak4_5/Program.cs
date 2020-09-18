428057c02ecdad2c061775277f7fb780804137ff
Aniska31 <Avstralia08@mail.ru>
2020-09-18 11:44:13 +0300

    finish

--- prak4_5/Program.cs
+++ prak4_5/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿//Создание обобщенного набора для хранения информации о государствах
+using System;
+using System.Collections.Generic;
 
 namespace prak4_5
+{ }
+class Program
 {
-  class Program
+  static void Main(string[] args)
   {
-    static void Main(string[] args)
+    Dictionary <int, String> countryLookup = new Dictionary<int, String>();
+    countryLookup[44] = "United Kingdom"; countryLookup[33] = "France";
+    countryLookup[31] = "Netherlands"; countryLookup[55] = "Brazil";
+    //countryLookup["64"] = "New Zealand";
+    Console.WriteLine("The 33 Code is for: {0}", countryLookup[33]);
+    foreach (KeyValuePair<int, String> item in countryLookup)
     {
-      Console.WriteLine("Hello World!");
+      int code = item.Key;
+      string country = item.Value;
+      Console.WriteLine("Code {0} = {1}", code, country);
     }
+    Console.Read();
   }
 }
