b26a61f49f5916d2729d2b1c8516344dd330bd6a
Aniska31 <Avstralia08@mail.ru>
2020-09-18 10:03:11 +0300

    finish

--- prak3/Program.cs
+++ prak3/Program.cs
@@ -8,27 +8,34 @@ namespace prak3
     {
       public static void Main()
       {
-        string pattern = @"[+]{0,1}[7]{0,1}[8]{0,1}[(]{0,1}\d{3}[)]{0,1}[-]{0,1}\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}";
-        string input = @"
-        +79511357545
-        +7(951)1357545
-        +7(951)-135-75-45
-        (951)1357545
-        (951)-135-75-45
-        89511357545
-        8(951)1357545
-        8(951)-135-75-45";
-        RegexOptions options = RegexOptions.Multiline;
-
-        foreach (Match m in Regex.Matches(input, pattern, options))
+        string find_my_number = @"[+]{0,1}[7]{0,1}[8]{0,1}[(]{0,1}\d{3}[)]{0,1}[-]{0,1}\d{3}[-]{0,1}\d{2}[-]{0,1}\d{2}";// регулятор моего телефона
+        string[] text = {
+        "+79511357545",
+        "+7(951)1357545",
+        "+7(951)-135-75-45",
+        "(951)1357545",
+        "(951)-135-75-45",
+        "89511357545",
+        "8(951)1357545",
+        "8(951)-135-75-45",
+        "ЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫ"};
+        foreach (string item in text)
         {
-          Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+          string pattern = @"[+]{0,1}[7]{0,1}[8]{0,1}[(]{0,1}\d{3}[)]{0,1}";
+          if (Regex.IsMatch(item, pattern))
+          {
+            Console.WriteLine(item + " это телефон");
+          }
+          else
+          {
+            Console.WriteLine(
+            string.Format("{0} это НЕ телефон", item)
+            );
+          }
+
         }
+
       }
-    }
-    static void Main(string[] args)
-    {
-      
     }
   }
 }
