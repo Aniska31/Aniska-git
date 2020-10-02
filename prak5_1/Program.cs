635e0235cc2d79f35fd70a0a62af1c40f817051b
Aniska31 <Avstralia08@mail.ru>
2020-10-02 09:14:59 +0300

    class

--- prak5_1/Program.cs
+++ prak5_1/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace prak5_1
+namespace Serialize_People
 {
-  class Program
+  class Person
   {
-    static void Main(string[] args)
+    public string name;
+    public DateTime dateOfBirth;
+    public int age;
+
+    public Person(string _name, DateTime _dateOfBirth)
     {
-      Console.WriteLine("Hello World!");
+      name = _name;
+      dateOfBirth = _dateOfBirth;
+      CalculateAge();
+    }
+
+    public Person()
+    {
+    }
+
+    public override string ToString()
+    {
+      return name + " was born on " + dateOfBirth.ToShortDateString() + " and is " + age.ToString() + " years old.";
+    }
+
+    private void CalculateAge()
+    {
+      age = DateTime.Now.Year - dateOfBirth.Year;
+
+      // If they haven't had their birthday this year, 
+      // subtract a year from their age
+      if (dateOfBirth.AddYears(DateTime.Now.Year - dateOfBirth.Year) > DateTime.Now)
+      {
+        age--;
+      }
     }
   }
 }
