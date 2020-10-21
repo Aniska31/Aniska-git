3023d8ad81b4d237b887e053a2d5879c26cd03d4
aniska31 <Avstralia08@mail.ru>
2020-10-21 18:45:51 +0300

    Создание очереди рабочих элементов при помощи ThreadPool

--- prak8_3/Program.cs
+++ prak8_3/Program.cs
@@ -1,18 +1,24 @@
-﻿using System;
+﻿//Практикум. Создание очереди рабочих элементов при помощи ThreadPool
+using System;
 using System.Threading;
 
 namespace prak8_3
 {
     class Program
     {
-        static void ShowHyText(object state)
+        static void ShowMyText(object state)
         {
             string myText = (string)state;
-            Console.WriteLine("Thread: (0) - (1)", Thread.CurrentThread.ManagedThreadId, myText);
+            Console.WriteLine("Thread: {0} - {1}", Thread.CurrentThread.ManagedThreadId, myText);
     }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WaitCallback callback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(callback, "Hello");
+            ThreadPool.QueueUserWorkItem(callback, "Hi");
+            ThreadPool.QueueUserWorkItem(callback, "Heya");
+            ThreadPool.QueueUserWorkItem(callback, "Goodbye");
+            Console.Read();
         }
     }
 }
