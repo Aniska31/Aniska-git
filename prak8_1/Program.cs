1a2c8a1e4c6f8a0b7641b426b4e716fa3b560027
aniska31 <Avstralia08@mail.ru>
2020-10-21 18:24:33 +0300

    static void Counting()

--- prak8_1/Program.cs
+++ prak8_1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Создание нескольких потоков
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
@@ -7,6 +8,15 @@ namespace prak8_1
 {
     class Program
     {
+        static void Counting()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Count: {0} - Thread' {1} ", i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
