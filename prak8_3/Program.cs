2b6c73444a00a3ab0a8db7bfb4e4f7f5e36b84fc
aniska31 <Avstralia08@mail.ru>
2020-10-21 18:42:05 +0300

    static void ShowHyText(object state)

--- prak8_3/Program.cs
+++ prak8_3/Program.cs
@@ -5,6 +5,11 @@ namespace prak8_3
 {
     class Program
     {
+        static void ShowHyText(object state)
+        {
+            string myText = (string)state;
+            Console.WriteLine("Thread: (0) - (1)", Thread.CurrentThread.ManagedThreadId, myText);
+    }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
