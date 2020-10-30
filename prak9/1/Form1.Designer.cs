80051c3e11c15f3e3e062bab235f4a2ba3af5c8e
Aniska31 <Avstralia08@mail.ru>
2020-10-30 10:14:26 +0300

    answers

--- prak9/1/Form1.Designer.cs
+++ prak9/1/Form1.Designer.cs
@@ -30,13 +30,14 @@
     {
       this.label1 = new System.Windows.Forms.Label();
       this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
       this.SuspendLayout();
       // 
       // label1
       // 
       this.label1.AutoSize = true;
       this.label1.Font = new System.Drawing.Font("Swis721 BlkOul BT", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label1.Location = new System.Drawing.Point(71, 172);
+      this.label1.Location = new System.Drawing.Point(71, 184);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(315, 29);
       this.label1.TabIndex = 0;
@@ -45,19 +46,31 @@
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(76, 317);
+      this.button1.Font = new System.Drawing.Font("Swis721 BlkEx BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.Location = new System.Drawing.Point(76, 325);
       this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.Size = new System.Drawing.Size(91, 34);
       this.button1.TabIndex = 1;
-      this.button1.Text = "button1";
+      this.button1.Text = "Безусловно!";
       this.button1.UseVisualStyleBackColor = true;
       this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.Font = new System.Drawing.Font("Swis721 BlkEx BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2.Location = new System.Drawing.Point(246, 325);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(140, 34);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "Ни в коем случаи!";
+      this.button2.UseVisualStyleBackColor = true;
       // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(453, 444);
+      this.Controls.Add(this.button2);
       this.Controls.Add(this.button1);
       this.Controls.Add(this.label1);
       this.Name = "Form1";
@@ -71,6 +84,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
