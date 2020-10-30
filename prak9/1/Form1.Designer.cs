037ab2cfa4bc2466de0d9b4b405163be75814a9c
Aniska31 <Avstralia08@mail.ru>
2020-10-30 10:16:05 +0300

    OXY

--- prak9/1/Form1.Designer.cs
+++ prak9/1/Form1.Designer.cs
@@ -31,6 +31,8 @@
       this.label1 = new System.Windows.Forms.Label();
       this.button1 = new System.Windows.Forms.Button();
       this.button2 = new System.Windows.Forms.Button();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.textBox2 = new System.Windows.Forms.TextBox();
       this.SuspendLayout();
       // 
       // label1
@@ -65,11 +67,29 @@
       this.button2.Text = "Ни в коем случаи!";
       this.button2.UseVisualStyleBackColor = true;
       // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(12, 33);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(41, 20);
+      this.textBox1.TabIndex = 3;
+      this.textBox1.Text = "X:";
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(12, 70);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(41, 20);
+      this.textBox2.TabIndex = 4;
+      this.textBox2.Text = "Y:";
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(453, 444);
+      this.Controls.Add(this.textBox2);
+      this.Controls.Add(this.textBox1);
       this.Controls.Add(this.button2);
       this.Controls.Add(this.button1);
       this.Controls.Add(this.label1);
@@ -85,6 +105,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
