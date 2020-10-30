3f7d22aff58c71a90e07117c1640cdaf8a02105f
Aniska31 <Avstralia08@mail.ru>
2020-10-30 11:43:02 +0300

    toolbar - buttons

--- prak9/2/2/Form1.Designer.cs
+++ prak9/2/2/Form1.Designer.cs
@@ -28,6 +28,7 @@
     /// </summary>
     private void InitializeComponent()
     {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
       this.menuStrip1 = new System.Windows.Forms.MenuStrip();
       this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -36,7 +37,11 @@
       this.форматToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.форматPNGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+      this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+      this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+      this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
       this.menuStrip1.SuspendLayout();
+      this.toolStrip1.SuspendLayout();
       this.SuspendLayout();
       // 
       // menuStrip1
@@ -99,11 +104,45 @@
       // 
       this.toolStrip1.AutoSize = false;
       this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+      this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3});
       this.toolStrip1.Location = new System.Drawing.Point(0, 24);
       this.toolStrip1.Name = "toolStrip1";
       this.toolStrip1.Size = new System.Drawing.Size(44, 426);
       this.toolStrip1.TabIndex = 1;
       this.toolStrip1.Text = "toolStrip1";
+      // 
+      // toolStripButton1
+      // 
+      this.toolStripButton1.AutoSize = false;
+      this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+      this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.toolStripButton1.Name = "toolStripButton1";
+      this.toolStripButton1.Size = new System.Drawing.Size(42, 42);
+      this.toolStripButton1.Text = "toolStripButton1";
+      // 
+      // toolStripButton2
+      // 
+      this.toolStripButton2.AutoSize = false;
+      this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+      this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.toolStripButton2.Name = "toolStripButton2";
+      this.toolStripButton2.Size = new System.Drawing.Size(42, 42);
+      this.toolStripButton2.Text = "toolStripButton2";
+      // 
+      // toolStripButton3
+      // 
+      this.toolStripButton3.AutoSize = false;
+      this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+      this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+      this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+      this.toolStripButton3.Name = "toolStripButton3";
+      this.toolStripButton3.Size = new System.Drawing.Size(42, 44);
+      this.toolStripButton3.Text = "toolStripButton3";
       // 
       // Form1
       // 
@@ -117,6 +156,8 @@
       this.Text = "Form1";
       this.menuStrip1.ResumeLayout(false);
       this.menuStrip1.PerformLayout();
+      this.toolStrip1.ResumeLayout(false);
+      this.toolStrip1.PerformLayout();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -132,6 +173,9 @@
         private System.Windows.Forms.ToolStripMenuItem форматToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem форматPNGToolStripMenuItem;
         private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
     }
 }
 
