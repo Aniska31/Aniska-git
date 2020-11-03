3ff3d037bc20e3cc02609387b106af06343e2492
aniska31 <Avstralia08@mail.ru>
2020-11-03 17:05:46 +0300

    form

--- prak10/2/Form1.Designer.cs
+++ prak10/2/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(604, 461);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Однорукий Бандит";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
