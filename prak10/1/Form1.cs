3d1483a972eaad51a0d00e2b53f08c483fd5b2df
aniska31 <Avstralia08@mail.ru>
2020-11-03 12:02:01 +0300

    change timer

--- prak10/1/Form1.cs
+++ prak10/1/Form1.cs
@@ -12,9 +12,18 @@ namespace _1
 {
     public partial class Form1 : Form
     {
+        //Добавляем экземпляр класса Random, он нам понадобиться для получения слуйчаной буквы.
+        Random random = new Random();
         public Form1()
         {
             InitializeComponent();
+            timer1.Enabled = true; //Делаем таймер активным
+            timer1.Start(); //Запускаем таймер 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
