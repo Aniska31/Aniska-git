291edd82d0196a1bea2be9b8c41704ada24669e5
aniska31 <Avstralia08@mail.ru>
2020-11-03 17:18:18 +0300

    labels and buttons

--- prak10/2/Form1.cs
+++ prak10/2/Form1.cs
@@ -16,5 +16,15 @@ namespace _2
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
