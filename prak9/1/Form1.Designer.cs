f14b7e1bd2c7cf200be110399a12bb9b3826db3f
aniska31 <Avstralia08@mail.ru>
2020-11-03 17:55:33 +0300

    rebranding

--- prak9/1/Form1.Designer.cs
+++ prak9/1/Form1.Designer.cs
@@ -28,102 +28,106 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.Question = new System.Windows.Forms.Label();
-      this.Yes = new System.Windows.Forms.Button();
-      this.No = new System.Windows.Forms.Button();
-      this.OX = new System.Windows.Forms.TextBox();
-      this.OY = new System.Windows.Forms.TextBox();
-      this.X = new System.Windows.Forms.Label();
-      this.Y = new System.Windows.Forms.Label();
-      this.SuspendLayout();
-      // 
-      // Question
-      // 
-      this.Question.AutoSize = true;
-      this.Question.Font = new System.Drawing.Font("Swis721 BlkOul BT", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.Question.Location = new System.Drawing.Point(71, 184);
-      this.Question.Name = "Question";
-      this.Question.Size = new System.Drawing.Size(324, 29);
-      this.Question.TabIndex = 0;
-      this.Question.Text = "Вы хотите попить пивка?!";
-      this.Question.Click += new System.EventHandler(this.label1_Click);
-      // 
-      // Yes
-      // 
-      this.Yes.Font = new System.Drawing.Font("Swis721 BlkEx BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.Yes.Location = new System.Drawing.Point(76, 325);
-      this.Yes.Name = "Yes";
-      this.Yes.Size = new System.Drawing.Size(91, 34);
-      this.Yes.TabIndex = 1;
-      this.Yes.Text = "Безусловно!!!";
-      this.Yes.UseVisualStyleBackColor = true;
-      this.Yes.Click += new System.EventHandler(this.button1_Click);
-      // 
-      // No
-      // 
-      this.No.Font = new System.Drawing.Font("Swis721 BlkEx BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.No.Location = new System.Drawing.Point(246, 325);
-      this.No.Name = "No";
-      this.No.Size = new System.Drawing.Size(140, 34);
-      this.No.TabIndex = 2;
-      this.No.Text = "Ни в коем случае!";
-      this.No.UseVisualStyleBackColor = true;
-      this.No.Click += new System.EventHandler(this.No_Click);
-      // 
-      // OX
-      // 
-      this.OX.Location = new System.Drawing.Point(38, 12);
-      this.OX.Name = "OX";
-      this.OX.Size = new System.Drawing.Size(49, 20);
-      this.OX.TabIndex = 3;
-      this.OX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-      this.OX.TextChanged += new System.EventHandler(this.OX_TextChanged);
-      // 
-      // OY
-      // 
-      this.OY.Location = new System.Drawing.Point(38, 49);
-      this.OY.Name = "OY";
-      this.OY.Size = new System.Drawing.Size(49, 20);
-      this.OY.TabIndex = 4;
-      this.OY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-      this.OY.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-      // 
-      // X
-      // 
-      this.X.AutoSize = true;
-      this.X.Location = new System.Drawing.Point(15, 15);
-      this.X.Name = "X";
-      this.X.Size = new System.Drawing.Size(17, 13);
-      this.X.TabIndex = 5;
-      this.X.Text = "X:";
-      // 
-      // Y
-      // 
-      this.Y.AutoSize = true;
-      this.Y.Location = new System.Drawing.Point(15, 52);
-      this.Y.Name = "Y";
-      this.Y.Size = new System.Drawing.Size(17, 13);
-      this.Y.TabIndex = 6;
-      this.Y.Text = "Y:";
-      // 
-      // Best_Form
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(453, 444);
-      this.Controls.Add(this.Y);
-      this.Controls.Add(this.X);
-      this.Controls.Add(this.OY);
-      this.Controls.Add(this.OX);
-      this.Controls.Add(this.No);
-      this.Controls.Add(this.Yes);
-      this.Controls.Add(this.Question);
-      this.Name = "Best_Form";
-      this.Text = "Предложение, от которого нельзя отказаться";
-      this.Load += new System.EventHandler(this.Form1_Load);
-      this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-      this.ResumeLayout(false);
-      this.PerformLayout();
+            this.Question = new System.Windows.Forms.Label();
+            this.Yes = new System.Windows.Forms.Button();
+            this.No = new System.Windows.Forms.Button();
+            this.OX = new System.Windows.Forms.TextBox();
+            this.OY = new System.Windows.Forms.TextBox();
+            this.X = new System.Windows.Forms.Label();
+            this.Y = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Question
+            // 
+            this.Question.AutoSize = true;
+            this.Question.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Question.Location = new System.Drawing.Point(71, 184);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(312, 29);
+            this.Question.TabIndex = 0;
+            this.Question.Text = "Вы хотите попить пивка?!";
+            this.Question.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Yes
+            // 
+            this.Yes.BackgroundImage = global::_1.Properties.Resources.da1;
+            this.Yes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Yes.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.Yes.Location = new System.Drawing.Point(76, 325);
+            this.Yes.Name = "Yes";
+            this.Yes.Size = new System.Drawing.Size(111, 34);
+            this.Yes.TabIndex = 1;
+            this.Yes.Text = "Безусловно!!!";
+            this.Yes.UseVisualStyleBackColor = true;
+            this.Yes.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // No
+            // 
+            this.No.BackgroundImage = global::_1.Properties.Resources.AK47jpg;
+            this.No.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.No.Location = new System.Drawing.Point(246, 325);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(153, 34);
+            this.No.TabIndex = 2;
+            this.No.Text = "Ни в коем случае!";
+            this.No.UseVisualStyleBackColor = true;
+            this.No.Click += new System.EventHandler(this.No_Click);
+            // 
+            // OX
+            // 
+            this.OX.Location = new System.Drawing.Point(38, 12);
+            this.OX.Name = "OX";
+            this.OX.Size = new System.Drawing.Size(49, 20);
+            this.OX.TabIndex = 3;
+            this.OX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.OX.TextChanged += new System.EventHandler(this.OX_TextChanged);
+            // 
+            // OY
+            // 
+            this.OY.Location = new System.Drawing.Point(38, 49);
+            this.OY.Name = "OY";
+            this.OY.Size = new System.Drawing.Size(49, 20);
+            this.OY.TabIndex = 4;
+            this.OY.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.OY.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // X
+            // 
+            this.X.AutoSize = true;
+            this.X.Location = new System.Drawing.Point(15, 15);
+            this.X.Name = "X";
+            this.X.Size = new System.Drawing.Size(17, 13);
+            this.X.TabIndex = 5;
+            this.X.Text = "X:";
+            // 
+            // Y
+            // 
+            this.Y.AutoSize = true;
+            this.Y.Location = new System.Drawing.Point(15, 52);
+            this.Y.Name = "Y";
+            this.Y.Size = new System.Drawing.Size(17, 13);
+            this.Y.TabIndex = 6;
+            this.Y.Text = "Y:";
+            // 
+            // Best_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_1.Properties.Resources.bg;
+            this.ClientSize = new System.Drawing.Size(453, 444);
+            this.Controls.Add(this.Y);
+            this.Controls.Add(this.X);
+            this.Controls.Add(this.OY);
+            this.Controls.Add(this.OX);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.Yes);
+            this.Controls.Add(this.Question);
+            this.Name = "Best_Form";
+            this.Text = "Предложение, от которого нельзя отказаться";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
