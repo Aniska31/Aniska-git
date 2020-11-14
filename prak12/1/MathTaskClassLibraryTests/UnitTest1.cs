71f3b17c58c80312dd09602ef18ca8809cca9313
aniska31 <avstralia08@mail.ru>
2020-11-14 17:01:37 +0300

    start working

---
+++ prak12/1/MathTaskClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathTaskClassLibrary;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
