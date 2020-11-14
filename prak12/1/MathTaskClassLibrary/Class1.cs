0b7d3276c8013d034c082a38496981b4297f1f49
aniska31 <avstralia08@mail.ru>
2020-11-14 16:46:01 +0300

    MathTaskClassLibrary

--- prak12/1/MathTaskClassLibrary/Class1.cs
+++ prak12/1/MathTaskClassLibrary/Class1.cs
@@ -1,8 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MathTaskClassLibrary
 {
-    public class Class1
+    public class Geometry
     {
+        public int RectangleArea(int a, int b)
+        {
+            return a * b;
+        }
     }
 }
