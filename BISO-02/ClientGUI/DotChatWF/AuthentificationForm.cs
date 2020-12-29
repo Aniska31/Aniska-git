f196e074ab23f9e87cbb0dedb1afbfd10d1358a8
aniska31 <avstralia08@mail.ru>
2020-12-29 14:29:22 +0300

    make auth(not done yet)

--- BISO-02/ClientGUI/DotChatWF/AuthentificationForm.cs
+++ BISO-02/ClientGUI/DotChatWF/AuthentificationForm.cs
@@ -12,14 +12,41 @@ namespace DotChatWF
 {
   public partial class AuthentificationForm : Form
   {
+    public class AuthData
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+
+    public MainForm mForm;
     public AuthentificationForm()
     {
-      InitializeComponent();
+        InitializeComponent();
     }
 
-    private void button1_Click(object sender, EventArgs e)
+    private void AuthentificationForm_Load(object sender, EventArgs e)
     {
 
     }
-  }
+
+    
+
+     private void button1_Click(object sender, EventArgs e)
+    {
+            string log = textBox1.Text;
+            string pas = textBox2.Text;
+
+    }
+
+    private void AuthentificationForm_FormClosing(object sender, FormClosingEventArgs e)
+    {
+
+    }
+
+    private void AuthentificationForm_FormClosed(object sender, FormClosedEventArgs e)
+    {
+        mForm.Show();
+        //this.Visible = false;
+    }
+    }
 }
