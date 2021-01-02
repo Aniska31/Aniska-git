617d808de3e57b67700be44fd09f1169346ec65e
aniska31 <avstralia08@mail.ru>
2021-01-02 15:03:31 +0300

    able to change timerate and size by config

--- BISO-02/ClientGUI/DotChatWF/MainForm.cs
+++ BISO-02/ClientGUI/DotChatWF/MainForm.cs
@@ -7,17 +7,67 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text;
+using System.Text.Json;
 
 namespace DotChatWF
 {
+    public struct Config
+    {
+        public int _Update_rate { get; set; }
+        public int _Height { get; set; }
+        public int _Width { get; set; }
 
+        public override string ToString()
+        {
+            return System.Text.Json.JsonSerializer.Serialize<Config>(this, new JsonSerializerOptions() { WriteIndented = true });
+        }
+    }
+
+    /*public class IMainFunction
+    {
+        /// <summary>
+        /// Преобразует объект в Json
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToJson(object value) =>
+            System.Text.Json.JsonSerializer.Serialize(value, new JsonSerializerOptions() { WriteIndented = true });
+
+        /// <summary>
+        /// Преобразует из Json в объект <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static T FromJson<T>(string value) =>
+            System.Text.Json.JsonSerializer.Deserialize<T>(value, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+
+        /// <summary>
+        /// Преобразует объект в Json и сохраняет в файл
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="value"></param>
+        public static void ToJsonFile(string path, object value) =>
+            File.WriteAllText(Path.Combine(Directory.GetCurrentDirectory(), path), ToJson(value));
+
+        /// <summary>
+        /// Считывает из файла json и преобразует в объект <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static T FromJsonFile<T>(string path) =>
+            FromJson<T>(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), path)));
+    }*/
     public partial class MainForm : Form
     {
+
         
         // Глобальные переменные
+        
         int lastMsgID = 0;
         AuthentificationForm AuthForm;
         RegistartionForm RegForm;
@@ -26,6 +76,7 @@ namespace DotChatWF
         public TextBox TextBox_text;
         public int int_token;
         public Message messages;
+        public static Config config;
 
 
         public MainForm()
@@ -96,17 +147,32 @@ namespace DotChatWF
         this.Visible = false;
         }
 
-    private void MainForm_Load(object sender, EventArgs e)
-    {
-      int_token = 0;
-      AuthForm = new AuthentificationForm();
-      RegForm = new RegistartionForm();
-      SmileForm = new Smiles();
-      TextBox_username = fieldUsername;
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            int_token = 0;
+            AuthForm = new AuthentificationForm();
+            RegForm = new RegistartionForm();
+            SmileForm = new Smiles();
+            TextBox_username = fieldUsername;
+            try
+            {
+                config = IMainFunction.FromJsonFile<Config>("config.json");
 
-    }
-
-    private void btnReg_Click(object sender, EventArgs e)
+            }
+            catch
+            {
+                config = new Config()
+                {
+                    _Width = 622,//622
+                    _Height = 441,//441
+                    _Update_rate = 5000//1000
+                };
+                IMainFunction.ToJsonFile("config.json", config);
+            }
+            Width = config._Width;
+            Height = config._Height;
+        }
+        private void btnReg_Click(object sender, EventArgs e)
     {
       RegForm.mForm = this;
       RegForm.Show();
